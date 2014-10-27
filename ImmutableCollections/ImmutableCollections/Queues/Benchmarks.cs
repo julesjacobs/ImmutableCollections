20413d5b993f73a8e2015deaec7768f064a32464
julesjacobs <julesjacobs@gmail.com>
2014-10-27 23:05:48 +0100

    Directory structure reorganization

---
+++ ImmutableCollections/ImmutableCollections/Queues/Benchmarks.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+
+namespace ImmutableCollections.Queues
+{
+    public class Benchmarks
+    {
+        public static void Run()
+        {
+            var n = 20;
+
+            Benchmark.Time("Collections.ImmutableQueue", () =>
+            {
+                var s = ImmutableQueue<int>.Empty;
+                s = s.Enqueue(n);
+                while (!s.IsEmpty)
+                {
+                    int k;
+                    s = s.Dequeue(out k);
+                    for (int i = 0; i < k; i++) s = s.Enqueue(i);
+                }
+            });
+
+            Benchmark.Time("DoubleStackQueue", () =>
+            {
+                var s = DoubleStackQueue<int>.Empty;
+                s = s.Enqueue(n);
+                while (!s.IsEmpty)
+                {
+                    int k;
+                    s = s.Dequeue(out k);
+                    for (int i = 0; i < k; i++) s = s.Enqueue(i);
+                }
+            });
+
+            Benchmark.Time("StackArrayQueue", () =>
+            {
+                var s = StackArrayQueue<int>.Empty;
+                s = s.Enqueue(n);
+                while (!s.IsEmpty)
+                {
+                    int k;
+                    s = s.Dequeue(out k);
+                    for (int i = 0; i < k; i++) s = s.Enqueue(i);
+                }
+            });
+
+            int size = 1000000;
+
+            var s1 = ImmutableQueue<int>.Empty;
+            Benchmark.Memory("Collections.ImmutableQueue", () =>
+            {
+                for (int i = 0; i < size; i++) s1 = s1.Enqueue(i);
+                int item;
+                s1 = s1.Dequeue(out item);
+                for (int i = 0; i < size; i++) s1 = s1.Enqueue(i);
+            });
+
+            var s2 = DoubleStackQueue<int>.Empty;
+            Benchmark.Memory("DoubleStackQueue", () =>
+            {
+                for (int i = 0; i < size; i++) s2 = s2.Enqueue(i);
+                int item;
+                s2 = s2.Dequeue(out item);
+                for (int i = 0; i < size; i++) s2 = s2.Enqueue(i);
+            });
+
+            var s3 = StackArrayQueue<int>.Empty;
+            Benchmark.Memory("StackArrayQueue", () =>
+            {
+                for (int i = 0; i < size; i++) s3 = s3.Enqueue(i);
+                int item;
+                s3 = s3.Dequeue(out item);
+                for (int i = 0; i < size; i++) s3 = s3.Enqueue(i);
+            });
+
+            // make sure stuff doesn't get GC'd during the measurement
+            Console.WriteLine(s1.IsEmpty);
+            Console.WriteLine(s2.IsEmpty);
+            Console.WriteLine(s3.IsEmpty);
+        }
+    }
+}
---
+++ ImmutableCollections/ImmutableCollections/Stacks/Benchmarks.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+
+namespace ImmutableCollections.Stacks
+{
+    public class Benchmarks
+    {
+        public static void Run()
+        {
+            var n = 25;
+
+            Benchmark.Time("Collections.ImmutableStack", () =>
+            {
+                var s = ImmutableStack<int>.Empty;
+                s = s.Push(n);
+                while (!s.IsEmpty)
+                {
+                    int k;
+                    s = s.Pop(out k);
+                    for (int i = 0; i < k; i++) s = s.Push(i);
+                }
+            });
+
+            Benchmark.Time("LinkedListStack", () =>
+            {
+                var s = LinkedListStack<int>.Empty;
+                s = s.Push(n);
+                while (!s.IsEmpty)
+                {
+                    int k;
+                    s = s.Pop(out k);
+                    for (int i = 0; i < k; i++) s = s.Push(i);
+                }
+            });
+
+            Benchmark.Time("NullLinkedListStack", () =>
+            {
+                var s = NullLinkedListStack<int>.Empty;
+                s = s.Push(n);
+                while (!s.IsEmpty)
+                {
+                    int k;
+                    s = s.Pop(out k);
+                    for (int i = 0; i < k; i++) s = s.Push(i);
+                }
+            });
+
+            Benchmark.Time("BufferedStack", () =>
+            {
+                var s = BufferedStack<int>.Empty;
+                s = s.Push(n);
+                while (!s.IsEmpty)
+                {
+                    int k;
+                    s = s.Pop(out k);
+                    for (int i = 0; i < k; i++) s = s.Push(i);
+                }
+            });
+
+            int size = 1000000;
+
+            var s1 = ImmutableStack<int>.Empty;
+            Benchmark.Memory("Collections.ImmutableStack", () =>
+            {
+                for (int i = 0; i < size; i++) s1 = s1.Push(i);
+            });
+
+            var s2 = LinkedListStack<int>.Empty;
+            Benchmark.Memory("LinkedListStack", () =>
+            {
+                for (int i = 0; i < size; i++) s2 = s2.Push(i);
+            });
+
+            var s3 = NullLinkedListStack<int>.Empty;
+            Benchmark.Memory("NullLinkedListStack", () =>
+            {
+                for (int i = 0; i < size; i++) s3 = s3.Push(i);
+            });
+
+            var s4 = BufferedStack<int>.Empty;
+            Benchmark.Memory("BufferedStack", () =>
+            {
+                for (int i = 0; i < size; i++) s4 = s4.Push(i);
+            });
+
+            // make sure stuff doesn't get GC'd during the measurement
+            Console.WriteLine(s1.IsEmpty);
+            Console.WriteLine(s2.IsEmpty);
+            Console.WriteLine(s3.IsEmpty);
+            Console.WriteLine(s4.IsEmpty);
+        }
+    }
+}
